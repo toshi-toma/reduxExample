f8c18a9bf21b76c717cd200fb30e0a1f4a6878a1
tosshyt <b1595204@planet.kanazawa-it.ac.jp>
2017-09-24 22:14:13 +0900

    todomvc add reducers todos.js

---
+++ todomvc/src/reducers/todos.js
@@ -0,0 +1,48 @@
+import { ADD_TODO, DELETE_TODO, EDIT_TODO, COMPLETE_TODO, COMPLETE_ALL, CLEAR_COMPLETED } from '../constants/ActionTypes'
+
+const initialState = [
+    {
+        text: 'Use Redux',
+        completes: false,
+        id: 0
+    }
+]
+
+export default function todos(state = initialState, action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
+                    completed: false,
+                    text: action.text
+                }
+            ]
+
+        case DELETE_TODO:
+            return state.filter(todo =>
+                todo.id != action.id
+            )
+        
+        case COMPLETE_TODO:
+            return state.map(todo =>
+                todo.id === action.id ?
+                { ...todo, completed: !todo.completed } :
+                todo
+            )
+
+        case COMPLETE_ALL:
+            const areAllMarked = state.every(todo => todo.completed)
+            return state.map(todo => ({
+                ...todo,
+                completed: !areAllMarked
+            }))
+
+        case CLEAR_COMPLETED:
+            return state.filter(todo => todo.completed === false)
+
+        default:
+            return state
+    }
+}
